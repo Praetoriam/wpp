ae2fbe31a49d3c7014ab8ba1c40f9714146b414c
Praetoriam <ositopanda10000@gmail.com>
2021-12-27 17:10:58 -0500

    Ordered with CSS.

--- JS-script.js
+++ JS-script.js
@@ -3,33 +3,29 @@ var jackpotnumber = Math.floor(Math.random() * 501);
 console.log(`Lucky number is: ${jackpotnumber}`);
 function addition() {
     const elmnt1 = document.getElementById('input-number').value;
-const elmnt2 = document.getElementById('input-number-two').value;
+    const elmnt2 = document.getElementById('input-number-two').value;
 
     const sum = parseInt(elmnt1) + parseInt(elmnt2);
     let total = document.getElementById('input-number-three').value = sum;
+    let result = document.getElementById('input-number-four')
+    let nan = Number(undefined);
+  
+    if (total == jackpotnumber) {
    
-
-   
-    
-    
-
-    if (total === jackpotnumber) {
-   
-        document.getElementById('input-number-four').value = "You win";
+        result.value = "YOU WIN!!";
     }
     else if (total != jackpotnumber) {
     
-        document.getElementById('input-number-four').value= "Sorry, try again";
+        result.value= "Sorry, you did not win.";
     }
-        
+    
 
     
 }
-
-
-
-
-    
-
-    
-  
+function deletefunction(){
+    let x = document.getElementsByClassName('input-text');
+    x[0].value="";
+    x[1].value="";
+    x[2].value="";
+    x[3].value="";
+}
